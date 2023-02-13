ed20f5b9fba8f515edeaee792201b08e7876c681
GodVilan <srikanthreddynandireddy@gmail.com>
2023-02-13 22:23:14 +0530

    serving html from server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("I'm Srikanth Reddy");
+	// res.send("I'm Srikanth Reddy");
+	res.sendFile(__dirname+"/index.html");
 });
 
 app.listen(port, function(){
