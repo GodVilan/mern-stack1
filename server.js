55191831d194e7fd31bddba5af0f0ad82e82b068
GodVilan <srikanthreddynandireddy@gmail.com>
2023-02-14 23:37:56 +0530

    added styles to page

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
-
+const options = {
+	extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
+	index:['index.html'],
+}
+app.use(express.static("public",options));
 app.get("/", function(req, res){
 	// res.send("I'm Srikanth Reddy");
 	res.sendFile(__dirname+"/index.html");
