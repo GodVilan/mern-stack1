9f4181c8b4f4a9ab77d6ef6021b7b4e85fc72d44
GodVilan <srikanthreddynandireddy@gmail.com>
2023-02-15 22:03:26 +0530

    added styles to page

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -20,9 +22,17 @@ app.get("/card", function(req, res){
 	// res.send("I'm Srikanth Reddy");
 	res.sendFile(__dirname+"/card.html");
 });
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
+	if(err){
+		console.error(err);
+	}
+	else{
+		console.log("DB Connected");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+		
+	}
 });
-
